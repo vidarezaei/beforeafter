3f125b40be9417db1bbc20161594428107a38aef
vidarezaei <vida.rezaei21@gmail.com>
2025-04-20 01:24:44 +0330

    parent of img(div) deleted

--- src/Component/BeforeAfter.jsx
+++ src/Component/BeforeAfter.jsx
@@ -5,29 +5,31 @@ function BeforeAfter({ beforeimg, afterimg }) {
   const [borderValue, setBorderValue] = useState(50);
   const [draggingState, setDraggingState] = useState(false);
 
+  window.addEventListener("mouseup", draggingStop);
+  window.addEventListener("mousedown", draggingStart);
+
+
+
+
+
 
   function updateBorderValue(e) {
-
   const target = document.querySelector(".container");
   const rect = target.getBoundingClientRect();
 
-    const newValue = Math.min(
-      Math.max(((e.clientX - rect.left) / target.offsetWidth) * 100, 0),
-      100
-    );
+  const newValue =Math.min(
+    Math.max(((e.clientX - rect.left) / target.offsetWidth) * 100, 0),
+    100
+  );
 
    
     if (draggingState){
       setBorderValue(newValue);
     } 
-
-
- 
   }
 
 
-  window.addEventListener("mouseup", draggingStop);
-  window.addEventListener("mousedown", draggingStart);
+  
 
   function draggingStart() {
     setDraggingState(true);
@@ -51,15 +53,12 @@ function BeforeAfter({ beforeimg, afterimg }) {
 
   return (
     <div className="container" >
-      <div className="beforeImgcs" style={{ clipPath: `polygon(0 0 ,  ${borderValue}%  0 , ${borderValue}%  100% , 0 100% )`}}  >
-        <img src={beforeimg} alt="beforeimg" />
-      </div>
-
-      <div className="afterImgcs">
-        <img src={afterimg} alt="afterimg" />
-      </div>
-
-      <div className="border" style={{ left: `${borderValue}%` }} onMouseDown={draggingStart} onMouseUp={draggingStop}>
+      
+      <img src={beforeimg} alt="beforeimg" className="beforeImgcs" style={{ clipPath: `polygon(0 0 ,  ${borderValue}%  0 , ${borderValue}%  100% , 0 100% )`}} />
+      
+      <img src={afterimg} alt="afterimg" className="afterImgcs"/>
+      
+      <div className="border" style={{ left: `${borderValue}%` }} onMouseDown={draggingStart} >
         <div className="borderBubble" />
       </div>
     </div>
