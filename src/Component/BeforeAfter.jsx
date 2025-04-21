e26df531785fa9eea07fc4241c0dc13719578be1
vidarezaei <vida.rezaei21@gmail.com>
2025-04-21 23:31:00 +0330

    some change

--- src/Component/BeforeAfter.jsx
+++ src/Component/BeforeAfter.jsx
@@ -1,37 +1,58 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./BeforeAfter.css";
-import updateBorderValue from "./functions/UpdateBorderValue";
 
 function BeforeAfter({ beforeimg, afterimg }) {
   const [borderValue, setBorderValue] = useState(50);
-  let moveHandler = null;
+  const [draggingState, setDraggingState] = useState(false);
+  const containerRef = useRef(null);
 
-  function draggingStart() {
-    moveHandler = updateBorderValue(setBorderValue);
-    window.addEventListener("mousemove", moveHandler);
-    window.addEventListener("mouseup", draggingStop);
+  function updateBorderValue(e) {
+    const containerWidth = containerRef.current.offsetWidth;
+    const containerLeft = containerRef.current.getBoundingClientRect().left;
+
+    const newValue = Math.min(
+      Math.max(((e.clientX - containerLeft) / containerWidth) * 100, 0),
+      100
+    );
+    setBorderValue(newValue);
+  }
+
+  function draggingStart(e) {
+    setDraggingState(true);
+    updateBorderValue(e);
   }
 
   function draggingStop() {
-    if (moveHandler) {
-      window.removeEventListener("mousemove", moveHandler);
-    }
-    window.removeEventListener("mouseup", draggingStop);
+    setDraggingState(false);
   }
-  
 
+  useEffect(() => {
+    if (draggingState) {
+      window.addEventListener("mousemove", updateBorderValue);
+      window.addEventListener("mouseup", draggingStop);
+    } else {
+      window.removeEventListener("mousemove", updateBorderValue);
+      window.removeEventListener("mouseup", draggingStop);
+    }
 
-
+    return () => {
+      window.removeEventListener("mousemove", updateBorderValue);
+      window.removeEventListener("mouseup", draggingStop);
+    };
+  }, [draggingState]);
 
   return (
-    <div className="container" >
+    <div className="container" ref={containerRef}>
+     
+        <img src={beforeimg} alt="beforeimg" className="beforeImgcs" style={{ clipPath: `polygon(0 0 ,  ${borderValue}%  0 , ${borderValue}%  100% , 0 100% )`}} />
       
-      <img src={beforeimg} alt="beforeimg" className="beforeImgcs" style={{ clipPath: `polygon(0 0 ,  ${borderValue}%  0 , ${borderValue}%  100% , 0 100% )`}} />
+
+     
+        <img src={afterimg} alt="afterimg" className="afterImgcs"/>
       
-      <img src={afterimg} alt="afterimg" className="afterImgcs"/>
-      
-      <div className="border" style={{ left: `${borderValue}%` }}   >
-        <div className="borderBubble"  onMouseDown={draggingStart} onMouseUp={draggingStop} />
+
+      <div className="border" style={{ left: `${borderValue}%` }} onMouseDown={draggingStart}>
+        <div className="borderBubble" />
       </div>
     </div>
   );
